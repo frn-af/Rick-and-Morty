6c3ff65efeab6bdd00bdfd7c93e2810671620ee5
frn-af <91farhanalfathra@gmail.com>
2024-04-30 03:54:31 +0700

    feat: shadcn ui (reinstall)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Rick and Morty",
@@ -16,8 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={
-        inter.variable}>
+      <body className={cn(
+        inter.className, "antialiased min-h-screen bg-background")}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-
+        <h1 className="text-4xl font-bold text-center">Rick and Morty</h1>
       </div>
     </main>
   );
